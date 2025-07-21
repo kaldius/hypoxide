8df26bfd3b6aded6c2823b6515e40aaf303c407f
Quan Teng Foong <quantf@gmail.com>
2025-07-21 09:05:17 +0800

    fix os exit code

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ pub fn test_runner(tests: &[&dyn Fn()]) {
     for test in tests {
         test();
     }
+    exit_qemu(QemuExitCode::Success);
 }
 
 #[test_case]
@@ -37,4 +38,22 @@ fn trivial_assertion() {
     print!("trivial assertion... ");
     assert_eq!(1, 1);
     println!("[ok]");
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)] // port size is 4 bytes (see cargo.toml iosize)
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
+
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+
+    unsafe {
+        // qemu isa-debug-exit lives at this port address (see cargo.toml iobase)
+        let mut port = Port::new(0xf4);
+        // qemu_exit_code will be (exit_code << 1) | 1
+        port.write(exit_code as u32);
+    }
 }
