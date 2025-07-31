95df7b3f47975f381f20ae559f7e9085a669e0f8
Quan Teng Foong <quantf@gmail.com>
2025-08-01 00:25:34 +0800

    catch stack overflow (double fault) by registering new stack in IST

--- src/main.rs
+++ src/main.rs
@@ -25,9 +25,11 @@ pub extern "C" fn _start() -> ! {
     // init IDT
     hypoxide::init();
 
-    unsafe {
-        *(0xdeadbeef as *mut u8) = 42;
+    fn stack_overflow() {
+        stack_overflow();
     }
+
+    stack_overflow();
 
     // test_main is only compiled when we call `cargo test`
     #[cfg(test)]
